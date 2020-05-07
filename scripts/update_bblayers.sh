58998a11e0c726583243828fb457332b14d31286
mingjian <mingjian@codeaurora.org>
2020-05-07 09:27:05 +0800

    meta-qti-connectivity: updated the scripts to support multi kernel Now the scripts can support kernel verrsion 4.9 and 4.14.98.
    Change-Id: I89eefcc836cd0c3973191d393ea1155a11d0c4fd

--- scripts/update_bblayers.sh
+++ scripts/update_bblayers.sh
@@ -56,6 +56,36 @@ EOF
 }
 
 
+bblayers_for_fsl_meta()
+{
+    cat >> ${BBLAYERS_CONF} <<EOF
+
+##Freescale Yocto Project Release layer
+BBLAYERS += " \${BSPDIR}/sources/meta-fsl-bsp-release/imx/meta-bsp "
+BBLAYERS += " \${BSPDIR}/sources/meta-fsl-bsp-release/imx/meta-sdk "
+BBLAYERS += " \${BSPDIR}/sources/meta-freescale-3rdparty "
+BBLAYERS += " \${BSPDIR}/sources/meta-freescale-distro "
+BBLAYERS += " \${BSPDIR}/sources/meta-freescale "
+EOF
+
+    case ${KERNELVERSION} in
+        "4.9")
+            echo "BBLAYERS += \" \${BSPDIR}/sources/poky/meta-poky \""  >> ${BBLAYERS_CONF}
+            ;;
+        "4.14")
+            echo "BBLAYERS += \" \${BSPDIR}/sources/poky/meta-poky \""  >> ${BBLAYERS_CONF}
+            ;;
+        "4.19")
+            echo "BBLAYERS += \" \${BSPDIR}/sources/poky/meta-poky \""  >> ${BBLAYERS_CONF}
+            echo "BBLAYERS += \" \${BSPDIR}/sources/meta-rust \"" >> ${BBLAYERS_CONF}
+            echo "BBLAYERS += \" \${BSPDIR}/sources/meta-fsl-bsp-release/imx/meta-ml \"" >> ${BBLAYERS_CONF}
+            ;;
+        *)
+            echo "Not supported kernel version ${KERNELVERSION}"
+            ;;
+        esac
+}
+
 bblayers_for_qca6574aule221()
 {
     cat >> ${BBLAYERS_CONF} <<EOF
@@ -90,7 +120,7 @@ EOF
 
 bblayers_for_qca6595aule01()
 {
-    bblayers_for_qca6574aule221
+    bblayers_for_fsl_meta
 }
 
 generate_QCA6584AULE201_bblayers()
