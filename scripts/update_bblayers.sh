4ad2a765bd4d2bcc23606ab6a9c520eaafc13230
mingjian <mingjian@codeaurora.org>
2020-05-27 19:52:37 +0800

    Unmask unused build scripts in meta-qti-connectivity to support multi kernel(4.14.98/4.9.88)building.
    Change-Id: I37462ce15cf44082d24545afdf6725283e38f663

--- scripts/update_bblayers.sh
+++ scripts/update_bblayers.sh
@@ -147,9 +147,26 @@ BBLAYERS = " \\
 EOF
 }
 
+KERNEL49="4.9"
+KERNEL414="4.14"
+EXCLUDE_KERNEL=""
+get_kernel_bbapend()
+{
+      EX_KERNEL_BBFILE=""
+      local DIR=${WORK_SPACE}/sources/meta-qti-connectivity/recipes-kernel/linux-kernel
+
+      if [ ! -d ${DIR} ]; then
+          echo "kernel dir not exist"
+          return 1
+      fi
+
+      EX_KERNEL_BBFILE="$(ls ${DIR} |grep "linux-imx_${EXCLUDE_KERNEL}"|grep "bbappend")"
+}
+
+
 bblayers_for_qti_meta()
 {
-  cat >> ${BBLAYERS_CONF} <<EOF
+    cat >> ${BBLAYERS_CONF} <<EOF
 
 ##QTI Yocto Connecetivity layer
 BBLAYERS += " \${BSPDIR}/sources/meta-qti-connectivity "
@@ -157,10 +174,30 @@ BBLAYERS += " \${BSPDIR}/sources/meta-qti-connectivity-prop "
 
 EOF
 
-#Update BBMASK bbfiles
-if [ -f "${WORK_SPACE}/sources/meta-qti-connectivity/conf/standalone-bbmask.conf" ]; then
-    cat ${WORK_SPACE}/sources/meta-qti-connectivity/conf/standalone-bbmask.conf >> ${BBLAYERS_CONF}
-fi
+    #Update BBMASK bbfiles
+    if [ -f "${WORK_SPACE}/sources/meta-qti-connectivity/conf/standalone-bbmask.conf" ]; then
+        cat ${WORK_SPACE}/sources/meta-qti-connectivity/conf/standalone-bbmask.conf >> ${BBLAYERS_CONF}
+    fi
+
+    case ${KERNELVERSION} in
+        "4.9")
+            EXCLUDE_KERNEL=${KERNEL414}
+            get_kernel_bbapend
+            if [ ["${EX_KERNEL_BBFILE}" != ""] ]; then
+                echo "BBMASK.=\"|meta-qti-connectivity/recipes-kernel/linux-kernel/${EX_KERNEL_BBFILE}\"" >> ${BBLAYERS_CONF}
+            fi
+            ;;
+        "4.14")
+            EXCLUDE_KERNEL=${KERNEL49}
+            get_kernel_bbapend
+            if [ ["${EX_KERNEL_BBFILE}" != ""] ]; then
+                echo "BBMASK.=\"|meta-qti-connectivity/recipes-kernel/linux-kernel/${EX_KERNEL_BBFILE}\"" >> ${BBLAYERS_CONF}
+            fi
+            ;;
+        *)
+            echo "Not supported kernel version ${KERNELVERSION}"
+            ;;
+        esac
 
 }
 
