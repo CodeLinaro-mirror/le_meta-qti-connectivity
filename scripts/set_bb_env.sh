60b465aa8b8c910142b6957d0abd807a6ce3d7a5
Zhaoyang Liu <zhaoyang@codeaurora.org>
2021-04-02 10:59:49 +0800

    meta-qti-connectivity: change url link for bzip file to fix build issue
    For specific bzip2 file, building server failed to download with http.
    Change to new URL link to avoid downloading failure in setup script.
    
    Change-Id: Iee3c305cbba4a2517562957da34f5e4b80349957
    Signed-off-by: Zhaoyang Liu <zhaoyang@codeaurora.org>

--- scripts/set_bb_env.sh
+++ scripts/set_bb_env.sh
@@ -193,6 +193,12 @@ else
     echo "ACCEPT_FSL_EULA = \"$EULA\"" >> conf/local.conf
 fi
 
+BZIP_BBFILE=${WORK_SPACE}/sources/poky/meta/recipes-extended/bzip2/bzip2_1.0.6.bb
+BZIP_NEW_URL="https://downloads.yoctoproject.org/mirror/sources/\${BP}.tar.gz"
+if grep -q 'SRC_URI = "http:' ${BZIP_BBFILE}; then
+    sed -i -e "s,SRC_URI = .*,SRC_URI = \"${BZIP_NEW_URL} \\\,g" ${BZIP_BBFILE}
+fi
+
 # Update bblayers.conf
 . ${WORK_SPACE}/${SCRIPT_FOLDER}/update_bblayers.sh ${PROJECTID}
 
