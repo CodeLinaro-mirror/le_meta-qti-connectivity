b1a3ca0fcbc23f082e144008334c3f3eec1148ba
Xueqiang Gong <quic_gongx@quicinc.com>
2022-01-11 11:04:49 +0800

    meta-qti-connectivity: add build script for WLAN HL solution for LK3_14
    Change-Id: Ibd58f0312a5c6c016b84f633b48028d36de23e00
    Signed-off-by: Xueqiang Gong <quic_gongx@quicinc.com>

--- scripts/prepare_sdk_3.14.28_os.sh
+++ scripts/prepare_sdk_3.14.28_os.sh
@@ -35,13 +35,13 @@ BBMASK.="|meta-qti-connectivity/recipes-kernel/linux-kernel/linux-msm-4.4_git.bb
 BBMASK.="|meta-qti-connectivity/recipes-kernel/lk/lk_git.bbappend"
 BBMASK.="|meta-qti-connectivity/recipes-kernel/linux-libc-headers/linux-libc-headers_%.bbappend"
 BBMASK.="|meta-qti-connectivity/recipes-kernel/linux-kernel/linux-imx_%.bbappend"
-BBMASK.="|meta-qti-connectivity/recipes-kernel/linux-kernel/linux-imx_3.14.28.bbappend"
+BBMASK.="|meta-qti-connectivity/recipes-kernel/linux-kernel/linux-imx_3.10.17.bbappend"
 ' >> conf/bblayers.conf
 
 sed -i -e 's/IMAGE_ROOTFS_SIZE ?= "131072"/IMAGE_ROOTFS_SIZE ??= "139264"/g' ../sources/poky/meta/conf/bitbake.conf
 
 echo  'MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += "pciutils"
-MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += "qcacld-ll"
+MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += "qcacld-hl"
 MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += "iw"
 MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += "iperf"
 MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += "wireless-tools"
