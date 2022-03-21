2ce268eebad6f1c090e5e6107930f272864e29bb
Xueqiang Gong <quic_gongx@quicinc.com>
2022-03-21 09:16:09 +0800

    meta-qti-connectivity: change the way to download code
    use https instead of git to download code
    
    Change-Id: Idedff8ddd4a275c8dc823eb9744a7aedb4fd94f5
    Signed-off-by: Xueqiang Gong <quic_gongx@quicinc.com>

--- scripts/extract_sourcecode.sh
+++ scripts/extract_sourcecode.sh
@@ -69,11 +69,11 @@ download_git_code()
 
 META_FSL_GIT="git://git.yoctoproject.org/meta-freescale"
 
-META_FSL_3RD_GIT="git://github.com/Freescale/meta-freescale-3rdparty"
+META_FSL_3RD_GIT="https://github.com/Freescale/meta-freescale-3rdparty"
 
-META_FSL_DIS_GIT="git://github.com/Freescale/meta-freescale-distro"
+META_FSL_DIS_GIT="https://github.com/Freescale/meta-freescale-distro"
 
-META_FSL_BROWSER_GIT="git://github.com/OSSystems/meta-browser"
+META_FSL_BROWSER_GIT="https://github.com/OSSystems/meta-browser"
 
 
 if [ $MACHINE == 'imx8mqevk' ]&&[ $PROJECTID == 'QCA6595AULE01' ]; then
