5a3e34bbd033fc8b692ae890f414d50c613a86e5
Dongyuan Li <quic_dongyuan@quicinc.com>
2023-08-18 16:00:07 +0800

    meta-qti-connectivity: Add umh support for fw ram dump
    Change-Id: I9c241bc4f6df0eef81721f9c7ef53ec4112b6b29
    Signed-off-by: Dongyuan Li <quic_dongyuan@quicinc.com>

---
+++ recipes-core/systemd/systemd-230/fw-ram-dump-umh.sh
@@ -0,0 +1,18 @@
+#Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+#SPDX-License-Identifier: BSD-3-Clause-Clear
+
+#!/bin/bash
+
+DEVPATH=$(readlink -f /sys/devices/virtual/devcoredump/devcd*)
+timestamp=$(date +%F_%H-%M-%S)
+dev=${DEVPATH##*/}
+if [ "$1" == "fw_rddm" ]; then
+filename=/var/crash/qca-fw-error_${dev}_${timestamp}.dump
+elif [ "$1" == "fw_sram" ]; then
+filename=/var/crash/qca-fw-sram_${dev}_${timestamp}.dump
+fi
+echo "${DEVPATH}" > ${filename}
+echo "\n" > ${filename}
+cat ${DEVPATH}/data > ${filename}
+sync ${filename}
+echo 1 > ${DEVPATH}/data
